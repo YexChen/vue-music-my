55132e0b8baea7b9a10fb1fc9fe2969b75c5a245
陈颖轩 <chenyingxuan@chenyingxuandeiMac.local>
2018-10-23 23:01:41 +0800

    init content added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+// 引入样式文件
+import 'assets/sass/index.sass';
+// fastclick插件
+import fastclick from 'fastclick';
+import 'babel-polyfill';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+fastclick.attach(document.body);
 Vue.config.productionTip = false;
 
 new Vue({
